9ac8cd4d906c3df45a58eec424698549ba184f82
nn6vgzby7x-cell <nn6vgzby7x@privaterelay.appleid.com>
2026-01-24 01:33:39 -0500

    Update App component message and simplify imports
    Fix App.js render

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));function App() {
+function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Trading Dashboard</h1>
-      <p>App loaded successfully.</p>
-    </div>
-  );
-}function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Trading Dashboard</h1>
-      <p>Live render test OK.</p>
+      <p>React app is running.</p>
     </div>
   );
 }
-
-export default App;
 
 export default App;
