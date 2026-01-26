666fc865d52923a04eb9b9309aefa0fedf24aee4
nn6vgzby7x-cell <nn6vgzby7x@privaterelay.appleid.com>
2026-01-26 13:02:18 -0500

    Update React rendering message in App component
    Fix React mount and App render

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Trading Dashboard</h1>
-      <p>If you can read this, React is rendering correctly.</p>
+      <p>If you can see this, React is working.</p>
     </div>
   );
 }
