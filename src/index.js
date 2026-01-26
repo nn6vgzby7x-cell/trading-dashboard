fbd3ea292f164d2fba96d4598bc4377b14d480c1
nn6vgzby7x-cell <nn6vgzby7x@privaterelay.appleid.com>
2026-01-26 09:30:45 -0500

    Remove CSS styles and set up React app
    Fix syntax error in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Arial, sans-serif;
-  background: white;
-  color: black;
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
