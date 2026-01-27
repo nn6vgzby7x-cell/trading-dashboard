78c4d188cd68eb158cf9154beb43b02b2429a7ab
nn6vgzby7x-cell <nn6vgzby7x@privaterelay.appleid.com>
2026-01-27 08:49:06 -0500

    Update App.js
    Add App component render

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-src/App.js
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Trading Dashboard</h1>
+      <p>React is rendering correctly.</p>
+    </div>
+  );
+}
